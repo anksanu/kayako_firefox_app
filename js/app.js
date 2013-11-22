12c27074cc1b3f637de0b6524871985d1b2c604f
Ankit Agrawal <agrawal.anksanu@gmail.com>
2013-11-23 00:55:05 +0530

    added ajax call for login into helpdesk

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
 $('#login-button').on('click', function(){
 	alert('lets begin with the api intergration dude !!');
+	$.ajax({
+		url : 'http://ekansh.kayako.com/staffapi/index.php?/Core/Default/Login',
+		type : 'post',
+		data : 'username=admin&password=changeit',
+		headers: { 
+        Accept : "xml",
+        "Content-Type": "xml"
+    },
+		contentType : 'application/x-www-form-urlencoded',
+		success : function(response){
+			alert(response);
+		},
+		error : function(){
+			alert('unable to login');
+		}
+	}).complete(function(){
+		alert('this call is completed');
+	});
 });
